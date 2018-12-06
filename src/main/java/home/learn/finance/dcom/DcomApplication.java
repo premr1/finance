6fab5a1af63b8f3bc3e47088a8a4240a1a200bfb
Prem Rishikumaran <premr1@gmail.com>
2018-12-06 05:21:47 +0000

    First commit

---
+++ src/main/java/home/learn/finance/dcom/DcomApplication.java
@@ -0,0 +1,12 @@
+package home.learn.finance.dcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcomApplication.class, args);
+	}
+}
